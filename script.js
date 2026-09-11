b6aa4b8f6290308286c6a8072c76f2574fe07e99
shs2211158.aposaga@csav.edu.ph <shs2211158.aposaga@csav.edu.ph>
2026-09-11 09:19:55 +0800

    variables  ( let & const)

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
 console.log("Hello World");
-console.log("First Javascript");
+const myName = "john derence aposaga";
+let age = 20;
+const number = 9163214632;
+const address = "victorias city brgy 3 bat-us";
+
+console.log(`Name: ${myName}`);
+console.log(`Age: ${age}`);
+console.log(`Number: ${number}`);
+console.log(`Adress: ${address}`);
