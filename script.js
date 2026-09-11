3b262faff1f4e836a3888ef78d9460eee527899f
shs2211158.aposaga@csav.edu.ph <shs2211158.aposaga@csav.edu.ph>
2026-09-11 11:17:19 +0800

    querySelector

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ console.log(`Age: ${age}`);
 console.log(`Number: ${number}`);
 console.log(`Adress: ${address}`);
 
+
 //functions 
 
 function greet(name) {
@@ -31,6 +32,24 @@ function mdas(num1, num2) {
     let s = num1 - num2;
     return (`values: ${num1} and ${num2}\n product: ${m}\n quotient: ${d}\n sum: ${a}\n difference: ${s}`);
 
+
 }
 
 console.log(mdas(5, 3));
+
+const heading = document.querySelector("h1");
+console.log(heading);
+
+const contactHeading = document.querySelector("#contact h2");
+console.log(contactHeading);
+
+const servicesHeading = document.querySelector("#services h2");
+console.log(servicesHeading);
+
+
+
+
+
+
+
+
