b2830416ccc7b0a467b83fc92fc1a8737e81d81c
shs2211158.aposaga@csav.edu.ph <shs2211158.aposaga@csav.edu.ph>
2026-09-11 12:11:59 +0800

    addEventListener

--- script.js
+++ script.js
@@ -63,6 +63,32 @@ contactHeading.style.color = "purple";
 projectsHeading.style.color = "purple";
 servicesHeading.style.color = "purple";
 
+//event lsiteners
+
+heading.addEventListener("click", function () {
+
+    heading.style.color = "red";
+
+}
+
+);
+
+const toggleButton = document.querySelector('#switch');
+const body = document.querySelector('body');
+let isOff = false;
+
+toggleButton.addEventListener("click", function () {
+    isOff = !isOff;
+
+    if (isOff) {
+        body.style.backgroundColor = "black";
+        body.style.color = "white";
+
+    } else {
+        body.style.backgroundColor = "white";
+        body.style.color = "black";
+    }
+});
 
 
 
